df9b422356475c63201bba4a3b9e1367f62d960e
Skinnyworm <askinnyworm@gmail.com>
2015-06-19 14:42:19 +0800

    start to create content builder

--- example/models/Wechat.js
+++ example/models/Wechat.js
@@ -5,18 +5,15 @@ module.exports = function(Wechat){
   WechatService.bindApi(Wechat);
 
   WechatService.bindResponder(Wechat, function(responder){
-
     responder
+    .when({MsgType: 'location'}, function(message, reply){
+      
+    })
     .always(function(message, cb){
       this.messages.create({content: message}, cb);
     })
     .finally(function(message, reply){
-      reply({
-        msgType: 'text',
-        content: "Received :)"
-      });
+      reply.text("Received :p")
     });
-
   });
-
 };
